4a3a3d02a80dd28c3bbbb0d5cb0d7ad08369b23a
Juha Vierinen <jvierine@gmail.com>
2022-06-20 11:39:06 +0000

    added instructions on howto upload to pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 import setuptools
 
+# Howto
+#
+#This is how to upload a project to pipy. This is mainly a note to self, as this is the first project I have uplo#aded to pypi:
+#
+#python3 -m venv /tmp/venv
+#source /tmp/venv/bin/activate
+#pip install build
+#pip install twine
+#python3 -m build
+#python3 -m twine upload dist/ppigrf-1.0.0.tar.gz 
+
 #Genrate long description using readme file
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -8,7 +19,7 @@ setuptools.setup(
     name="ppigrf",
     version="1.0.0",
     author="Karl Laundal",
-    author_email="nan",
+    author_email="readme@file.md",
     description="Pure Python IGRF",
     long_description=long_description,
     long_description_content_type="text/markdown",
